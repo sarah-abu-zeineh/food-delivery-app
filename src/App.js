990c77dc72a900c4c4ae0729186de0f6d3fcc42a
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-01-13 00:22:51 +0200

    add routes for the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
+import CreateContainer from "./pages/CreateComponent";
+import MainContainer from "./pages/MainContainer";
 
 function App() {
   return (
     <div className="w-screen h-auto flex flex-col bg-primary">
     <Header/>
+    <main className="mt-24 p-8 w-full" >
+    <Routes>
+    <Route path='/*' element={<MainContainer/>}/>
+    <Route path='/createItem' element={<CreateContainer/>}/>
+    </Routes>
+    </main>
     </div>
   );
 }
