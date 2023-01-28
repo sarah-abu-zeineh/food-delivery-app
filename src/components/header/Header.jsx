e26ac650625cb46519500b86b3d473dc3de2a8c9
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-01-29 00:34:09 +0200

    create logOut function to log out when user click on logout icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -44,6 +44,14 @@ const Header = () => {
 
 
     }
+    const logOut =()=>{
+        setIsMenu(false);
+        localStorage.clear();
+        dispatch({
+            type:actionType.SET_USER,
+            user:null
+        })
+    }
 
     return (
         <div className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
@@ -188,7 +196,8 @@ const Header = () => {
                                 <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About Us</li>
                                 <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Service</li>
                             </ul>
-                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>LogOut
+                            <p className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-slate-200  gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base'
+                            onClick={logOut}>LogOut
                                 <MdLogout/></p>
                         </motion.div>
                     )
