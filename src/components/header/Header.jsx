cbf0072d39c52b245cc5a2c475cafee4c85544d5
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-01-30 22:50:40 +0200

    close the menu when clicking on any item in it

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -47,6 +47,7 @@ const Header = () => {
     const logOut = () => {
         setIsMenu(false);
         localStorage.clear();
+        console.log("sara");
         dispatch({type: actionType.SET_USER, user: null})
     }
 
@@ -128,12 +129,15 @@ const Header = () => {
                                 {
                                 user && user.email === 'sara.abu.zena2000@gmail.com' && (
                                     <Link to={'/createItem'}>
-                                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item
+                                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                                         onClick={()=>setIsMenu(false)}
+                                        >New Item
                                             <MdAdd/></p>
                                     </Link>
                                 )
                             }
-                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>LogOut
+                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                                onClick={logOut}>LogOut
                                     <MdLogout/></p>
                             </motion.div>
                         )
@@ -198,10 +202,10 @@ const Header = () => {
                             )
                         }
                             <ul className='flex flex-col'>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2 '>Home</li>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2 '>Menu</li>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About Us</li>
-                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Service</li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2 ' onClick={()=>setIsMenu(false)}>Home</li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2 ' onClick={()=>setIsMenu(false)}>Menu</li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={()=>setIsMenu(false)}>About Us</li>
+                                <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={()=>setIsMenu(false)}>Service</li>
                             </ul>
                             <p className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-slate-200  gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base'
                                 onClick={logOut}>LogOut
